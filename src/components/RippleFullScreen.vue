17475c1db3ab1f92198ebd01e617e525b835f272
dipsywong98 <facebookarmyoffcal@gmail.com>
2019-01-27 18:53:44 +0800

    ripple done

--- src/components/RippleFullScreen.vue
+++ src/components/RippleFullScreen.vue
@@ -1,7 +1,7 @@
 <template>
   <div ref="frame" class="frame">
     <div ref="text" class="text wave-effect waves-block" @click="toggle">this is some display text
-      <div ref="ripple" class="ripple">
+      <div ref="rippleContainer" class="rippleContainer">
         <div ref="realRipple" class="realRipple"></div>
       </div>
     </div>
@@ -9,28 +9,70 @@
   </div>
 </template>
 <script>
+import { setTimeout } from "timers";
 export default {
+  data() {
+    return {
+      position: {},
+      isActive: false,
+      isAnimating: false
+    };
+  },
+  mounted() {
+    const rippleContainer = this.$refs.rippleContainer;
+    rippleContainer.style.display = "none";
+  },
   methods: {
     toggle(e) {
       const { offsetX: x, offsetY: y } = e;
       console.log(x, y);
-      const rippleEl = this.$refs.ripple;
+      const rippleContainer = this.$refs.rippleContainer;
       const realRipple = this.$refs.realRipple;
-      if (realRipple.classList.contains("show")) {
-        realRipple.classList.remove("show");
-        setTimeout(() => {
-          realRipple.style.display = "none";
-          realRipple.classList.remove("anim");
-        }, 500);
-      } else {
-        rippleEl.style.left = x + "px";
-        rippleEl.style.top = y + "px";
-        realRipple.style.display = "";
-        console.log(rippleEl.classList);
-        // rippleEl.classList.push("anim", "show");
-        window.setTimeout(() => (realRipple.className += " anim show"), 1);
-        // rippleEl.className += "";
+      if (!this.isAnimating) {
+        this.isAnimating = true;
+        if (this.isActive) {
+          this.hide(x, y);
+        } else {
+          this.show(x, y);
+        }
       }
+    },
+    show(x, y) {
+      const rippleContainer = this.$refs.rippleContainer;
+      const realRipple = this.$refs.realRipple;
+      this.position.x = x;
+      this.position.y = y;
+      rippleContainer.style.left = this.position.x + "px";
+      rippleContainer.style.top = this.position.y + "px";
+      rippleContainer.style.display = "";
+      window.setTimeout(() => {
+        rippleContainer.style.left = null;
+        rippleContainer.style.top = null;
+        realRipple.className += " anim show";
+        rippleContainer.className += " anim show";
+      }, 1);
+      setTimeout(() => {
+        this.isActive = true;
+        this.isAnimating = false;
+      }, 500);
+    },
+    hide(x, y) {
+      const rippleContainer = this.$refs.rippleContainer;
+      const realRipple = this.$refs.realRipple;
+      console.log("position", this.position);
+      setTimeout(() => {
+        rippleContainer.style.left = this.position.x + "px";
+        rippleContainer.style.top = this.position.y + "px";
+      }, 1);
+      realRipple.classList.remove("show");
+      rippleContainer.classList.remove("show");
+      setTimeout(() => {
+        rippleContainer.style.display = "none";
+        realRipple.classList.remove("anim");
+        rippleContainer.classList.remove("anim");
+        this.isActive = false;
+        this.isAnimating = false;
+      }, 500);
     }
   }
 };
@@ -47,9 +89,11 @@ export default {
 .content {
   display: none;
 }
-.ripple {
+.rippleContainer {
   position: absolute;
   border: #000000 solid;
+  max-width: 100vw;
+  max-height: 100vh;
 }
 .realRipple {
   position: absolute;
@@ -65,10 +109,14 @@ export default {
 .anim {
   transition: all 0.5s cubic-bezier(0.4, 0, 0.2, 1);
 }
-.show {
+.realRipple.show {
   transform: scale(1);
   top: -100vh;
   left: -100vw;
+}
+.rippleContainer.show {
+  top: 50vh;
+  left: 50vw;
 }
 </style>
 
