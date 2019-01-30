63094d76aec4304017a1935b77437d91f99eba90
dipsywong98 <facebookarmyoffcal@gmail.com>
2019-01-30 15:13:11 +0800

    play with parallex perspective change

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,15 @@
     </RippleFullScreen>
     <!-- <Rippify>
       <div>x</div>
-    </Rippify> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </Rippify>-->
+    <div class="perspectiveContainer">
+      <div :style="`
+      transform: rotateX(${rx}deg);
+    `" ref="ctn">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
+    <div style="width:100%;height:100vh"></div>
   </div>
 </template>
 
@@ -27,6 +34,24 @@ export default {
     HelloWorld,
     RippleFullScreen,
     Rippify
+  },
+  data() {
+    return { rx: 0 };
+  },
+  methods: {
+    handleScroll(e) {
+      console.log(window.scrollY);
+      this.rx = Math.min(
+        (window.scrollY / this.$refs.ctn.clientHeight) * 110,
+        110
+      );
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 </script>
@@ -39,5 +64,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.perspectiveContainer {
+  perspective: 300px;
+  position: relative;
+  padding: 3vh 4vw;
+  z-index: 1;
+  -webkit-perspective: 300px;
+  perspective: 300px;
+  -webkit-filter: drop-shadow(0 0.5vh 1vh rgba(0, 0, 0, 0.3));
+  filter: drop-shadow(0 0.5vh 1vh rgba(0, 0, 0, 0.3));
+  perspective-origin: top;
 }
 </style>
