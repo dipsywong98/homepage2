5d86e2d8dd1442d274719292d3f6d18303d269f5
dipsywong98 <ycwongal@connect.ust.hk>
2020-01-15 00:12:10 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
+mkdir __tmp
+if [ ! -d "dist/"]; then
+  git remote get-url origin > $REMOTE
+  mkdir dist
+  cd dist
+  git init
+  git remote add origin $REMOTE
+  cd ..
+fi
+mv dist/.git/ __tmp/.git/
 npm run build
+mv __tmp/.git/ dist/.git/
+rm -r __tmp/
 
-cd dist || echo "no build found" || exit
+cd dist || echo "not build found" || exit
 
 git add .
 git commit -am "deploy"
-git push -fu origin master
+#git push -fu origin master
