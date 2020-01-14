8e77ed9d49b98f1339273fa9afcd0385ff55ca86
dipsywong98 <ycwongal@connect.ust.hk>
2020-01-15 00:41:05 +0800

    handle deploy script for the first run

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,24 @@
 mkdir __tmp
-if [ ! -d "dist/"]; then
-  git remote get-url origin > $REMOTE
+if [ ! -d 'dist/' ]; then
+  echo 'dist/ not exist, create dist/'
   mkdir dist
+fi
+if [ ! -d 'dist/.git/' ]; then
+  echo 'dist is not a git repo. Initializing it as a git repo'
+  REMOTE=$(git remote get-url origin)
   cd dist
   git init
-  git remote add origin $REMOTE
+  git remote add origin "$REMOTE"
   cd ..
 fi
-mv dist/.git/ __tmp/.git/
-npm run build
-mv __tmp/.git/ dist/.git/
-rm -r __tmp/
+
+mv dist/.git/ __tmp/.git/ &&
+npm run build &&
+mv __tmp/.git/ dist/.git/ &&
+rm -r __tmp/ || echo "move or build fail" || exit
 
 cd dist || echo "not build found" || exit
 
 git add .
 git commit -am "deploy"
-#git push -fu origin master
+git push -fu origin master
