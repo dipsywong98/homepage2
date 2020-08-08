650df8e5741fb0ca14d97ceb0e1ce80a6edd5d5f
dipsywong98 <ycwongal@connect.ust.hk>
2020-08-08 16:23:24 +0800

    extract name card data to elsewhere

--- src/components/NameCard.vue
+++ src/components/NameCard.vue
@@ -1,57 +1,64 @@
 <template lang="pug">
   .card
     .field
-      img(src="/img/icon.png" width="204px")
+      img(:src="my.icon" width="204px")
     .field
-      h1 Dipsy Wong
-      h3 Year4 BEng(COMP & MATH), HKUST
-      h5 A random hacky programmer who loves making anything fun!
+      h1 {{my.name}}
+      h3 {{my.position}}
+      h5 {{my.tagLine}}
       .links
-        SvgLink(href="//www.facebook.com/FK.twf/" icon="facebook" color="#1877F2" title="Facebook")
-        SvgLink(href="//github.com/dipsywong98" icon="github" color="#181717" title="GitHub")
-        SvgLink(href="//www.linkedin.com/in/dipsywong/" icon="linkedin" color="0077B5" title="LinkedIn")
-        SvgLink(href="mailto:ycwongal@connect.ust.hk" icon="gmail" color="D14836" title="Email")
-        SvgLink(href="//www.youtube.com/user/FacebookArmyOffical" icon="youtube" color="#FF0000" title="YouTube")
+        SvgLink(v-for="medium in my.media" :key="medium.title" :href="medium.href" :icon="medium.icon" :color="medium.color" :title="medium.title")
 
 </template>
 <style lang="scss" scoped>
-  .card-section {
-    // width: 100%;
-    // min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
+.card-section {
+  // width: 100%;
+  // min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
-  .card {
-    position: relative;
-    background-color: var(--bright);
-    min-height: 300px;
-    max-width: 1000px;
-    margin: calc((var(--section-height) - 300px) / 2) auto;
-    box-shadow: 2px 2px 8px 4px rgba(0, 0, 0, 0.25);
-    @media screen and (max-width: 600px) {
-      margin: 10rem 0;
-    }
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    flex-wrap: wrap;
+.card {
+  position: relative;
+  background-color: var(--bright);
+  min-height: 300px;
+  max-width: 1000px;
+  margin: calc((var(--section-height) - 300px) / 2) auto;
+  box-shadow: 2px 2px 8px 4px rgba(0, 0, 0, 0.25);
+  @media screen and (max-width: 600px) {
+    margin: 10rem 0;
   }
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
 
-  .field {
-    margin: 20px;
-  }
+.field {
+  margin: 20px;
+}
 
-  .links {
-    display: flex;
-    justify-content: space-around;
-  }
+.links {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
 <script>
 import SvgLink from './SvgLink'
+import jsyaml from 'js-yaml'
 
 export default {
-  components: { SvgLink }
+  components: { SvgLink },
+  async beforeCreate() {
+    this.my = jsyaml.load(await (await fetch('https://hackmd.io/ux_QXxWNTbetEKqOgRSQSA/download')).text())
+  },
+  data() {
+    return {
+      my: {
+        media: []
+      }
+    }
+  }
 }
 </script>
